ebc1a918cff36bb289ed920fe9768c6c141a39f1
lamboshe <awilisean@gmail.com>
2026-04-10 14:14:48 +0000

    git commit "  Add index.html"             "  Add style.css "             "  Add script.js "

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Fake movie images (you can replace with API later)
+// real movie images (you can replace with API later)
 const trendingMovies = [
   "https://via.placeholder.com/200x300?text=Movie+1",
   "https://via.placeholder.com/200x300?text=Movie+2",
@@ -37,4 +37,51 @@ function getStarted() {
   } else {
     alert("Welcome! " + email);
   }
+}
+// Fake movie database
+const movies = [
+  "Avengers",
+  "Batman",
+  "Spiderman",
+  "Interstellar",
+  "Titanic",
+  "Joker"
+];
+
+function searchMovie() {
+  const input = document.getElementById("searchInput").value.toLowerCase();
+  const resultsDiv = document.getElementById("results");
+
+  // filter movies
+  const results = movies.filter(movie =>
+    movie.toLowerCase().includes(input)
+  );
+
+  // show results
+  if (results.length === 0) {
+    resultsDiv.innerHTML = "<p>No movies found ❌</p>";
+  } else {
+    resultsDiv.innerHTML = results
+      .map(movie => `<p>🎬 ${movie}</p>`)
+      .join("");
+  }
+}
+
+function searchMovie() {
+  const input = document.getElementById("searchInput").value;
+
+  // save in browser memory
+  localStorage.setItem("lastSearch", input);
+
+  alert("Saved: " + input);
+}
+
+const last = localStorage.getItem("lastSearch");
+console.log("Last search was:", last);
+
+function goToSearch() {
+  const query = document.getElementById("searchInput").value;
+
+  // send user to another page with query
+  window.location.href = `search.html?q=${query}`;
 }
